97ebf5f1ab44a9d934800524310fe7e0b9e9d30d
Leo Gutierrez <leo.gutierrez.ext@kinship.co>
2025-01-22 19:42:32 -0600

    Added test coverage for User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "friendly_name returns full name if first name is present" do
+    user = User.new(first_name: "Leo", last_name: "Doe", email: "john.doe@example.com")
+    assert_equal "Leo Doe", user.friendly_name
+  end
+
+  test "friendly_name returns email if first name is not present" do
+    user = User.new(first_name: nil, last_name: nil, email: "leo.doe@example.com")
+    assert_equal "leo.doe@example.com", user.friendly_name
+  end
 end
