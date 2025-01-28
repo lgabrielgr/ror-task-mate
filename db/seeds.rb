c7e50d5b15ebf65658ad3e8569cea2140bb37286
Leo Gutierrez <leo.gutierrez.ext@kinship.co>
2025-01-27 18:39:26 -0600

    Rubocop fix: Layout/TrailingEmptyLines: 1 trailing blank lines detected.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,3 @@
 User.create!(first_name: "Leo", last_name: "Doe", email: "leo.doe@test.com", password: "zaq12wsx")
 User.create!(first_name: "Gabriel", last_name: "Doe", email: "gabriel.doe@test.com", password: "zaq12wsx")
 User.create!(first_name: "Lula", last_name: "Doe", email: "lula.doe@test.com", password: "zaq12wsx")
-
