22c7f55fcf26f7e6904413c2af96b77b2c49bc96
Leo Gutierrez <leo.gutierrez.ext@kinship.co>
2025-01-29 18:44:53 -0600

    Fix rubocop errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,4 +19,3 @@ Ticket.create!(title: "Project Scaffolding", description: "Description of Ticket
 Ticket.create!(title: "Create endpoints to retrieve user information", description: "Description of Ticket 3", status: 0, team_id: Team.find_by(name: "Java API").id, assignee_id: User.find_by(email: "lula.doe@test.com").id)
 Ticket.create!(title: "Implement basic auth", description: "Description of Ticket 4", status: 0, team_id: Team.find_by(name: "Java API").id, assignee_id: User.find_by(email: "zuly.doe@test.com").id)
 Ticket.create!(title: "Project Scaffolding", description: "Description of Ticket 5", status: 3, team_id: Team.find_by(name: "Java API").id, assignee_id: User.find_by(email: "zuly.doe@test.com").id)
-
