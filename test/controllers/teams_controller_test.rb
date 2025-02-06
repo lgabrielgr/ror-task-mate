2a9aa88dd0a677f13120b44465194c6df6c469a9
Leo Gutierrez <leo.gutierrez.ext@kinship.co>
2025-02-05 18:48:08 -0600

    Added authorization to render teams tickets view only if current user is assigned or is the owner of the team

--- test/controllers/teams_controller_test.rb
+++ test/controllers/teams_controller_test.rb
@@ -1,14 +1,26 @@
 require "test_helper"
 
 class TeamsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    post new_user_session_path, params: { user: { email: users(:one).email, password: "password" } }
-  end
+  include Devise::Test::IntegrationHelpers
 
   test "should get team info and tickets" do
+    sign_in users(:one)
     team = teams(:one)
     get team_tickets_url(team_id: team.id)
     assert_response :success
     assert_not_nil assigns(:tickets)
+  end
+
+  test "should redirect if user cannot see team tickets" do
+    sign_in users(:two) # Assuming users(:two) cannot see team tickets
+    team = teams(:two)
+    get team_tickets_url(team_id: team.id)
+    assert_redirected_to root_path
+  end
+
+  test "should log warning and redirect if team does not exist" do
+    sign_in users(:one)
+    get team_tickets_url(team_id: -1)
+    assert_redirected_to root_path
   end
 end
