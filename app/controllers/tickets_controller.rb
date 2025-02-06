0e4a18d8d991023c9753f957c9b02332fd1585fa
Leo Gutierrez <leo.gutierrez.ext@kinship.co>
2025-02-05 18:48:27 -0600

    Added authorization to render tickets view only if current user is assigned or is the owner of the team

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,22 @@
 class TicketsController < ApplicationController
+  before_action :authorize_user, only: [ :view ]
+
   def view
     @ticket = Ticket.find(params[:id])
     @team = @ticket.team
+  end
+
+  private
+
+  def authorize_user
+    begin
+      @ticket = Ticket.find(params[:id])
+      unless current_user.can_see_ticket?(@ticket)
+        redirect_to root_path
+      end
+    rescue ActiveRecord::RecordNotFound
+      Rails.logger.warn "User with id #{current_user.id} tried to access non-existent ticket with id #{params[:id]}"
+      redirect_to root_path
+    end
   end
 end
