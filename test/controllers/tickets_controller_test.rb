5389993cca966a5947322258886b3d79ba575308
Leo Gutierrez <leo.gutierrez.ext@kinship.co>
2025-02-06 19:22:28 -0600

    Add test coverage for ticket edit functionality

--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -20,4 +20,22 @@ class TicketsControllerTest < ActionDispatch::IntegrationTest
     get ticket_view_url(id: 4)
     assert_redirected_to root_path
   end
+
+  test "should get edit if user is assigned to team" do
+    sign_in users(:one)
+    get ticket_edit_url(id: 1)
+    assert_response :success
+  end
+
+  test "should redirect to root if user cannot see and edit ticket" do
+    sign_in users(:two)
+    get ticket_edit_url(id: 4)
+    assert_redirected_to root_path
+  end
+
+  test "should update ticket if user is assigned to team" do
+    sign_in users(:one)
+    patch ticket_url(id: 1), params: { ticket: { title: "Updated Title", description: "Updated Description" } }
+    assert_redirected_to ticket_view_path(id: 1)
+  end
 end
