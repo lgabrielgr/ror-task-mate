b928d600baf80cecba0a6f8b6eaa90fab12c1dea
Leo Gutierrez <leo.gutierrez.ext@kinship.co>
2025-02-28 20:56:38 -0600

    feat: Setup of delayed_job for background jobs, i.e. emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,13 +32,22 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
 
   # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV.fetch('SMTP_SERVER'),
+    port:                 ENV.fetch('SMTP_PORT'),
+    domain:               ENV.fetch('SMTP_DOMAIN', 'taskmate.com'),
+    user_name:            ENV.fetch('SMTP_USERNAME'),
+    password:             ENV.fetch('SMTP_PASSWORD'),
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
